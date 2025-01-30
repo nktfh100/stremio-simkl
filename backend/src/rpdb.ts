45d59353bb272a2b5ddbc8cb3524e09a84070b60
nktfh100 <me@malachi.io>
2025-01-30 21:12:29 +0200

    Fix missing items in watching list, fix posters with 'returning series' type

--- backend/src/rpdb.ts
+++ backend/src/rpdb.ts
@@ -1,5 +1,25 @@
+import { CleanedTMDBMovie, CleanedTMDBShow } from "./types";
+
 const RPDB_API = "https://api.ratingposterdb.com";
 
-export function getRPDBPosterUrl(imdbId: string): string {
+export function generateRPDBPosterUrl(imdbId: string): string {
   return `${RPDB_API}/${process.env.RPDB_API_KEY}/imdb/poster-default/${imdbId}.jpg`;
+}
+
+// RPDB only has released movies and series
+export function mediaHasRPDBPoster(
+  mediaType: "movie" | "series",
+  tmdbMeta: CleanedTMDBMovie | CleanedTMDBShow | null,
+): boolean {
+  if (!tmdbMeta) return false;
+
+  if (tmdbMeta.status == "Released") return true;
+
+  if (tmdbMeta.status == "In Production") return false;
+
+  if (mediaType == "series" && (tmdbMeta as CleanedTMDBShow)?.last_air_date) {
+    return true;
+  }
+
+  return false;
 }
