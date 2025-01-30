fc21c73c8243a7321e23a5425bba117534598029
Malachi <me@nktfh100.com>
2025-01-30 20:42:46 +0200

    Use imdb ID for rpdb instead of tmdb (#16)

--- backend/src/rpdb.ts
+++ backend/src/rpdb.ts
@@ -1,8 +1,5 @@
 const RPDB_API = "https://api.ratingposterdb.com";
 
-export function getRPDBPosterUrl(
-  tmdbId: string,
-  mediaType: "movie" | "series",
-): string {
-  return `${RPDB_API}/${process.env.RPDB_API_KEY}/tmdb/poster-default/${mediaType}-${tmdbId}.jpg`;
+export function getRPDBPosterUrl(imdbId: string): string {
+  return `${RPDB_API}/${process.env.RPDB_API_KEY}/imdb/poster-default/${imdbId}.jpg`;
 }
