32c39ca33d392f55d1a615f7d7c186d3b8f47767
nktfh100 <me@malachi.io>
2025-01-30 21:19:41 +0200

    Bump version

--- backend/src/generateManifest.ts
+++ backend/src/generateManifest.ts
@@ -1,75 +1,75 @@
 import { slugify } from "@/utils";
 
 const catalogExtra = [
-	{
-		name: "skip",
-		isRequired: false,
-	},
+  {
+    name: "skip",
+    isRequired: false,
+  },
 ];
 
 export default function generateManifest(
-	user: string,
-	configured: boolean = true
+  user: string,
+  configured: boolean = true,
 ) {
-	let description = `Unofficial addon to display your Simkl Watchlists in Stremio, by @nktfh100`;
+  let description = `Unofficial addon to display your Simkl Watchlists in Stremio, by @nktfh100`;
 
-	if (!user) {
-		user = "unknown";
-	}
+  if (!user) {
+    user = "unknown";
+  }
 
-	if (configured) {
-		description += ` (SIMKL user - ${user})`;
-	}
+  if (configured) {
+    description += ` (SIMKL user - ${user})`;
+  }
 
-	let id = "com.nktfh100.stremiosimkl";
+  let id = "com.nktfh100.stremiosimkl";
 
-	if (configured) {
-		id += "." + slugify(user);
-	}
+  if (configured) {
+    id += "." + slugify(user);
+  }
 
-	return {
-		id,
-		version: "0.2.1",
-		name: "Simkl Watchlists",
-		description,
-		logo: "https://eu.simkl.in/img_favicon/v2/favicon-192x192.png",
-		catalogs: [
-			{
-				id: "simkl-plan-to-watch-movie",
-				type: "movie",
-				name: "SIMKL Plan To Watch",
-				extra: catalogExtra,
-			},
-			{
-				id: "simkl-completed-movie",
-				type: "movie",
-				name: "SIMKL Completed",
-				extra: catalogExtra,
-			},
-			{
-				id: "simkl-watching",
-				type: "series",
-				name: "SIMKL Watching",
-				extra: catalogExtra,
-			},
-			{
-				id: "simkl-plan-to-watch-series",
-				type: "series",
-				name: "SIMKL Plan To Watch",
-				extra: catalogExtra,
-			},
-			{
-				id: "simkl-completed-series",
-				type: "series",
-				name: "SIMKL Completed",
-				extra: catalogExtra,
-			},
-		],
-		resources: ["catalog"],
-		types: ["movie", "series"],
-		behaviorHints: {
-			configurable: !configured,
-			configurationRequired: !configured,
-		},
-	};
+  return {
+    id,
+    version: "0.2.2",
+    name: "Simkl Watchlists",
+    description,
+    logo: "https://eu.simkl.in/img_favicon/v2/favicon-192x192.png",
+    catalogs: [
+      {
+        id: "simkl-plan-to-watch-movie",
+        type: "movie",
+        name: "SIMKL Plan To Watch",
+        extra: catalogExtra,
+      },
+      {
+        id: "simkl-completed-movie",
+        type: "movie",
+        name: "SIMKL Completed",
+        extra: catalogExtra,
+      },
+      {
+        id: "simkl-watching",
+        type: "series",
+        name: "SIMKL Watching",
+        extra: catalogExtra,
+      },
+      {
+        id: "simkl-plan-to-watch-series",
+        type: "series",
+        name: "SIMKL Plan To Watch",
+        extra: catalogExtra,
+      },
+      {
+        id: "simkl-completed-series",
+        type: "series",
+        name: "SIMKL Completed",
+        extra: catalogExtra,
+      },
+    ],
+    resources: ["catalog"],
+    types: ["movie", "series"],
+    behaviorHints: {
+      configurable: !configured,
+      configurationRequired: !configured,
+    },
+  };
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,38 +6,38 @@ import styles from "@/App.module.scss";
 import useAppStore from "@/lib/appStore";
 
 export default function App() {
-	const simklCode = useAppStore((state) => state.code);
-	const installLink = useAppStore((state) => state.installLink);
+  const simklCode = useAppStore((state) => state.code);
+  const installLink = useAppStore((state) => state.installLink);
 
-	return (
-		<div className={styles["app"]}>
-			<div className={styles["content"]}>
-				<div className={styles["logos"]}>
-					<img src="/stremio-logo.svg" alt="Stremio Logo" />
-					<p>+</p>
-					<img src="/simkl-logo.webp" alt="Simkl Logo" />
-				</div>
-				<h1 className={styles["title"]}>Stremio Simkl Watchlists</h1>
-				<h2 className={styles["version"]}>v0.2.1</h2>
+  return (
+    <div className={styles["app"]}>
+      <div className={styles["content"]}>
+        <div className={styles["logos"]}>
+          <img src="/stremio-logo.svg" alt="Stremio Logo" />
+          <p>+</p>
+          <img src="/simkl-logo.webp" alt="Simkl Logo" />
+        </div>
+        <h1 className={styles["title"]}>Stremio Simkl Watchlists</h1>
+        <h2 className={styles["version"]}>v0.2.2</h2>
 
-				<h2 className={styles["description"]}>
-					<Balancer>
-						Unofficial Stremio addon to display your Simkl Watchlists in Stremio
-					</Balancer>
-				</h2>
+        <h2 className={styles["description"]}>
+          <Balancer>
+            Unofficial Stremio addon to display your Simkl Watchlists in Stremio
+          </Balancer>
+        </h2>
 
-				<div className={styles["button-container"]}>
-					{!simklCode && <SimklAuth />}
+        <div className={styles["button-container"]}>
+          {!simklCode && <SimklAuth />}
 
-					{simklCode && !installLink ? <GenerateLink /> : null}
+          {simklCode && !installLink ? <GenerateLink /> : null}
 
-					{simklCode && installLink ? (
-						<LinkBtn href={installLink} colorsReversed={true}>
-							Install
-						</LinkBtn>
-					) : null}
-				</div>
-			</div>
-		</div>
-	);
+          {simklCode && installLink ? (
+            <LinkBtn href={installLink} colorsReversed={true}>
+              Install
+            </LinkBtn>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
