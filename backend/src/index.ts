b618d3c1d8d838c44bc1edefb40f8fa8b7b1d78d
nktfh100 <me@malachi.io>
2025-02-04 17:51:58 +0200

    Add simple metrics

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,7 @@ import registerGenerateLinkRoute from "@/routes/generateLink";
 import registerManifestRoute from "@/routes/manifest";
 
 import registerConfigureRoute from "./routes/configure";
+import { initMetrics, metricsEndpoint, metricsMiddleware } from "./metrics";
 
 // import { publishToCentral } from "stremio-addon-sdk";
 
@@ -23,12 +24,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.redirect(process.env.FRONTEND_URL!);
+if (process.env.ENABLE_METRICS) {
+  initMetrics();
+  app.use(metricsMiddleware);
+  app.get("/metrics", metricsEndpoint);
+}
+
+app.get("/", (_req, res) => {
+  res.redirect(process.env.FRONTEND_URL!);
 });
 
-app.get("/health", (req, res) => {
-	res.send("OK");
+app.get("/health", (_req, res) => {
+  res.send("OK");
 });
 
 registerManifestRoute(app);
@@ -37,19 +44,19 @@ registerGenerateLinkRoute(app);
 registerCatalogRoute(app);
 
 app.listen(PORT, async () => {
-	console.log(`Server listening on port ${PORT}`);
-	if (!process.env.DISABLE_REDIS) {
-		connectToRedis();
-	}
+  console.log(`Server listening on port ${PORT}`);
+  if (!process.env.DISABLE_REDIS) {
+    connectToRedis();
+  }
 
-	// if (process.env.NODE_ENV == "production") {
-	// console.log("Publishing to central...");
-	// try {
-	// 	publishToCentral(
-	// 		`https://${process.env.BACKEND_HOST}/manifest.json`
-	// 	);
-	// } catch (error) {
-	// 	console.error("Failed to publish to central", error);
-	// }
-	// }
+  // if (process.env.NODE_ENV == "production") {
+  // console.log("Publishing to central...");
+  // try {
+  // 	publishToCentral(
+  // 		`https://${process.env.BACKEND_HOST}/manifest.json`
+  // 	);
+  // } catch (error) {
+  // 	console.error("Failed to publish to central", error);
+  // }
+  // }
 });
---
+++ backend/src/metrics.ts
@@ -0,0 +1,49 @@
+import { RequestHandler } from "express";
+import client from "prom-client";
+
+let register: client.Registry;
+
+export const getRegister = () => register;
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: "http_request_duration_ms",
+  help: "Duration of HTTP requests in ms",
+  labelNames: ["method", "route", "code"],
+  buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000, 10000],
+});
+
+const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "code"],
+});
+
+export const initMetrics = () => {
+  register = new client.Registry();
+
+  register.registerMetric(httpRequestDurationMicroseconds);
+  register.registerMetric(httpRequestCounter);
+};
+
+export const metricsEndpoint: RequestHandler = async (_req, res) => {
+  res.setHeader("Content-Type", getRegister().contentType);
+  res.send(await getRegister().metrics());
+};
+
+export const metricsMiddleware: RequestHandler = (req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on("finish", () => {
+    end({
+      route: req.route ? req.route.path : req.url,
+      code: res.statusCode,
+      method: req.method,
+    });
+
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.url,
+      code: res.statusCode,
+    });
+  });
+  next();
+};
